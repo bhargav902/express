0506cb62d89400fbfe533a5b6c008b71ff46f266
bhargav-busem <bhargav902@gmail.com>
2020-05-18 16:16:17 +0530

    first commit

---
+++ Members.js
@@ -0,0 +1,19 @@
+const members =[
+    {
+        id:1,
+        name:"bhargav",
+        email:"bhargav@gmail.com"
+    },
+    {
+        id:2,
+        name:"raju",
+        email:"raji@gmail.com"
+    },
+    {
+        id:3,
+        name:"jothi",
+        email:"jothi@gmail.com"
+    }
+];
+
+module.exports = members;
